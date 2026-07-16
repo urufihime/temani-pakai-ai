276f7b47ea40564a53f178ea3b4c4b8fe31f060f
urufihime <urufi.hime@gmail.com>
2026-07-16 20:15:10 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,47 @@
 import { auth } from "./firebase.js";
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
 console.log("✅ auth.js berhasil dimuat");
-console.log(auth);
+
+// Ambil elemen dari halaman
+const loginBtn = document.getElementById("loginBtn");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const errorDiv = document.getElementById("error");
+
+// Saat tombol login diklik
+loginBtn.addEventListener("click", async () => {
+
+    const email = emailInput.value.trim();
+    const password = passwordInput.value;
+
+    errorDiv.textContent = "";
+
+    try {
+
+        const userCredential = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        console.log("✅ Login berhasil");
+        console.log(userCredential.user);
+
+        // sementara tampilkan alert
+        alert("Login berhasil!");
+
+        // Nanti kita arahkan ke dashboard
+        // window.location.href = "dashboard.html";
+
+    } catch (error) {
+
+        console.error(error);
+
+        errorDiv.textContent = "Email atau password salah.";
+
+    }
+
+});
